a9d1315006ec94986f1a17faa50cb38373da2571
Bryan Edward <bryanedwardse@gmail.com>
2023-12-12 14:13:32 +0100

    versión 2.0 : Solucionado de problemas en los mensajes

--- PHPAuthSystem/ParaEntregar/mensajes_enviados.php
+++ PHPAuthSystem/ParaEntregar/mensajes_enviados.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('header.php');
 require('mensajes.php'); ?>
 <link rel="stylesheet" href="css/mensajes.css">
@@ -14,19 +14,21 @@ require('mensajes.php'); ?>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($msgEnviados as $mensaje): ?>
-                    <tr>
-                        <td>
-                            <?php echo $nombreDestino; ?>
-                        </td>
-                        <td>
-                            <?php echo $mensaje['contenido']; ?>
-                        </td>
-                        <td>
-                            <?php echo $mensaje['hora']; ?>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
+                <?php if (isset($_SESSION['id_user'])) {
+                    foreach ($msgEnviados as $mensaje): ?>
+                        <tr>
+                            <td>
+                                <?php echo $nombreDestino; ?>
+                            </td>
+                            <td>
+                                <?php echo $mensaje['contenido']; ?>
+                            </td>
+                            <td>
+                                <?php echo $mensaje['hora']; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach;
+                } ?>
             </tbody>
         </table>
     </div>
